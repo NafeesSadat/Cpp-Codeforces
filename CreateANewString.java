c90fc19155ebd3e4f8e4d273da6f35c82ae3ef76
NafeesSadat <92665005+NafeesSadat@users.noreply.github.com>
2022-03-09 20:59:48 +0600

    Add files via upload

---
+++ CreateANewString.java
@@ -0,0 +1,17 @@
+package create.a.pkgnew.string;
+
+import java.util.Scanner;
+
+
+public class CreateANewString {
+
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String st1=sc.next();
+        String st2=sc.next();
+        System.out.println(st1.length()+" "+st2.length());
+        System.out.println(st1+" "+st2);
+    }
+    
+}
