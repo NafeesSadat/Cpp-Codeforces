e1f8bacf1e7bf27673ddf935012e0923f7419606
NafeesSadat <92665005+NafeesSadat@users.noreply.github.com>
2022-03-09 22:11:54 +0600

    Add files via upload

---
+++ Way_too_long.java
@@ -0,0 +1,15 @@
+package way_too_long;
+
+import java.util.Scanner;
+
+public class Way_too_long {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String array[]= new String[100];
+        int a; 
+        a = input.nextInt();
+        
+    }
+    
+}
